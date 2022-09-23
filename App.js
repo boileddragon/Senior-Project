36889652f66fbc57b3fac50f5699e021e078ed7a
Faith Brown <faithbrown55@outlook.com>
2022-09-23 14:09:36 -0500

    :rocket: Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './app/screens/HomeView/Home.js';
+import LoginScreen from './app/screens/LoginView/Login.js';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
