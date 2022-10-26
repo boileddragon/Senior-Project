b3e7cbf3b8ce1837a12811dee3258050cf0fa2d3
Faith Brown <faithbrown55@outlook.com>
2022-10-26 18:46:44 -0500

    Removed map page

--- app/components/stacks/EventsStack.js
+++ app/components/stacks/EventsStack.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EventsScreen from "../../screens/tabbar-screens/EventsScreen.js";
 import CalendarScreen from "../../screens/other-screens/CalendarScreen.js";
-import MapScreen from "../../screens/other-screens/LocationScreen";
 
 
 const EventsStack = createNativeStackNavigator();
@@ -12,7 +11,6 @@ function EventsStackScreen() {
     <EventsStack.Navigator>
       <EventsStack.Screen name="What's Happening" component={EventsScreen} />
       <EventsStack.Screen name="Calendar" component={CalendarScreen} />
-      <EventsStack.Screen name="Map" component={MapScreen} />
     </EventsStack.Navigator>
   );
 }
