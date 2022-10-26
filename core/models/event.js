ae9c72e68f7608e9981592d9052520e917b8cbda
nealpatron <99371910+nealpatron@users.noreply.github.com>
2022-10-26 18:00:40 -0500

    Added code for Event class and repo

---
+++ core/models/event.js
@@ -0,0 +1,17 @@
+export class Event {
+    /**
+     * Event
+     * @param {string} title Event title
+     * @param {string} clubName Name of club that is hosting the event
+     * @param {string} details Any info about the event
+     * @param {string} location Location of the event
+     * @param {date} date Date and time of event
+     */
+    constructor(title, clubName, details, location, date) {
+        this.title = title,
+        this.clubName = clubName,
+        this.details = details,
+        this.location = location,
+        this.date = date
+    }
+}
---
+++ core/repos/eventRepo.js
@@ -0,0 +1,64 @@
+import { db } from "../firebaseConfig";
+import { Event } from "../models/event";
+
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+} from "firebase/firestore";
+
+export class EventRepo {
+  static db = db;
+  static collection = "Events";
+
+  /**
+   * Return all events a
+   * @returns 
+   */
+  static async all() {
+    let res = [];
+    const docs = await getDocs(
+      collection(db, EventRepo.collection).withConverter(eventConverter)
+    );
+    docs.forEach((doc) => {
+      res.push(doc.data());
+    });
+    //console.log(res);
+    return res;
+  }
+
+  /**
+   * Add event as new document
+   * @param {Event} event
+   * @returns 
+   */
+   static async add(event) {
+    return await addDoc(collection(db, EventRepo.collection).withConverter(eventConverter), event);
+  }
+}
+
+const eventConverter = {
+  toFirestore: (event) => {
+    return {
+      title: event.title,
+      clubName: event.clubName,
+      details: event.details,
+      location: event.location,
+      date: event.date,
+    };
+  },
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return new Event(
+      data.title,
+      data.clubName,
+      data.details,
+      data.location,
+      data.date
+    );
+  },
+};
